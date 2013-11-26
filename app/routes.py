36d1a112ccb6253f5ddb33b9cb3b88ac615b40ae
Jonathan Chew <jon.j.chew@gmail.com>
2013-11-25 16:16:58 -0800

    Adds post method for root route

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,6 @@ from flask import render_template
 from app import app
 from forms import Locator
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
     return render_template("index.html")
