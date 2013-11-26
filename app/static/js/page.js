f562e538875301435807aa8cac1429e2b45e0ae2
Jonathan Chew <jon.j.chew@gmail.com>
2013-11-26 01:15:32 -0800

    removes append spots, not needed anymore

--- app/static/js/page.js
+++ app/static/js/page.js
@@ -13,7 +13,6 @@ var Page = {
   			$.post('/', data, function(response){
   				global = response
         		var coordinatesArray = Page.getCoordinatesArray(response.results)
-        		// Page.appendSpots(response.results)
         		GoogleMaps.addMarkers(coordinatesArray)
     		});
 		});
@@ -28,7 +27,6 @@ var Page = {
   			$.post('/', data, function(response){
   				GoogleMaps.recenterMap(data)
         		var coordinatesArray = Page.getCoordinatesArray(response.results)
-        		// Page.appendSpots(response.results)
         		GoogleMaps.addMarkers(coordinatesArray)
     		});
 		});
@@ -39,19 +37,5 @@ var Page = {
 			coordinatesArray.push([responseArray[i]['latitude'], responseArray[i]['longitude'], responseArray[i]['name']])
 		}	
 		return coordinatesArray
-	},
-	appendSpots: function(spotsArray){
-		for(var i = 0; i < spotsArray.length; i++){
-			distance = spotsArray[i]['distance'].toFixed(2)
-			if(spotsArray[i]['name'] === '_undetermined') {
-				$('#spot-list').append("<li>" +	 spotsArray[i]['address'] + " (" + distance + "mi)" + "</li>")
-			}
-			else if(spotsArray[i]['address'] === 'None'){
-				$('#spot-list').append("<li>" +	 spotsArray[i]['name'] + " (" + distance + "mi)" + "</li>")
-			}
-			else{
-				$('#spot-list').append("<li>" +	 spotsArray[i]['name'] +  ", " + spotsArray[i]['address'] + " (" + distance + "mi)" + "</li>")
-			}
-		}
 	}
 }
