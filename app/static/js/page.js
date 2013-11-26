75d9652c39c91fe49aa2701745a85c1dad8c6259
Jonathan Chew <jon.j.chew@gmail.com>
2013-11-25 20:53:29 -0800

    Cleans up edge cases for dirty names and addresses

--- app/static/js/page.js
+++ app/static/js/page.js
@@ -26,7 +26,15 @@ var Page = {
 	},
 	appendSpots: function(spotsArray){
 		for(var i = 0; i < spotsArray.length; i++){
-			$('#spot-list').append("<li>" +	 spotsArray[i]['name'] +  "," + spotsArray[i]['address'] + " </li>")
+			if(spotsArray[i]['name'] === '_undetermined') {
+				$('#spot-list').append("<li>" +	 spotsArray[i]['address'] + " </li>")
+			}
+			else if(spotsArray[i]['address'] === 'None'){
+				$('#spot-list').append("<li>" +	 spotsArray[i]['name'] + "</li>")
+			}
+			else{
+				$('#spot-list').append("<li>" +	 spotsArray[i]['name'] +  ", " + spotsArray[i]['address'] + "</li>")
+			}
 		}
 	}
 }
