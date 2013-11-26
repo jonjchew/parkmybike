1f7415afb5ea5467903ac958fe1a61370fccc060
Jonathan Chew <jon.j.chew@gmail.com>
2013-11-25 23:16:42 -0800

    Remove forms.py

--- app/forms.py
+++
@@ -1,7 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import HiddenField
-from wtforms.validators import Required
-
-class Locator(Form):
-    longitude = HiddenField('longitude', validators = [Required()])
-    latitude = HiddenField('latitude', validators = [Required()])
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,10 @@
 import requests, json
 from flask import render_template, request, jsonify
 from app import app
-from forms import Locator
 from helpers import find_spots
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-	form = Locator()
 	if request.method == 'POST':
 		latitude = float(form.latitude.data)
 		longitude = float(form.longitude.data)
@@ -18,4 +16,4 @@ def index():
 
 		return jsonify(results = close_spots)
 	else:
-		return render_template("index.html", form = form)
+		return render_template("index.html")
