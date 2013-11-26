1e9cfb4a9039a3c0f2b6a9ea6b0808868b5af7f7
Jonathan Chew <jon.j.chew@gmail.com>
2013-11-25 21:03:13 -0800

    Adds distance to results, adjusts for signifcant figures

--- app/static/js/page.js
+++ app/static/js/page.js
@@ -26,14 +26,15 @@ var Page = {
 	},
 	appendSpots: function(spotsArray){
 		for(var i = 0; i < spotsArray.length; i++){
+			distance = spotsArray[i]['distance'].toFixed(2)
 			if(spotsArray[i]['name'] === '_undetermined') {
-				$('#spot-list').append("<li>" +	 spotsArray[i]['address'] + " </li>")
+				$('#spot-list').append("<li>" +	 spotsArray[i]['address'] + " (" + distance + "mi)" + "</li>")
 			}
 			else if(spotsArray[i]['address'] === 'None'){
-				$('#spot-list').append("<li>" +	 spotsArray[i]['name'] + "</li>")
+				$('#spot-list').append("<li>" +	 spotsArray[i]['name'] + " (" + distance + "mi)" + "</li>")
 			}
 			else{
-				$('#spot-list').append("<li>" +	 spotsArray[i]['name'] +  ", " + spotsArray[i]['address'] + "</li>")
+				$('#spot-list').append("<li>" +	 spotsArray[i]['name'] +  ", " + spotsArray[i]['address'] + " (" + distance + "mi)" + "</li>")
 			}
 		}
 	}
