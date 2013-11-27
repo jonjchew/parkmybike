8b6c6895fd44a88ae4942d53c4543e0a3a5c83e5
Jonathan Chew <jon.j.chew@gmail.com>
2013-11-26 17:37:11 -0800

    Adds test for GET index route

--- tests.py
+++ tests.py
@@ -14,9 +14,9 @@ class TestCase(unittest.TestCase):
         app.config['TESTING'] = True
         self.app = app.test_client()
 
-    # def test_locate_user_on_load(self):
-    #     rv = self.app.get('/')
-    #     assert 'Hello, World!' in rv.data
+    def test_get_index(self):
+        rv = self.app.get('/')
+        assert "<div id='map-canvas'>" in rv.data
 
     def test_helpers_find_spots(self):
     	close_spots = helpers.find_spots(origin, potential_spots)
