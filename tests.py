d441b8939a02782f54b481d00d135b79417bb590
Jonathan Chew <jon.j.chew@gmail.com>
2013-11-26 16:48:00 -0800

    writes first failing test

--- tests.py
+++ tests.py
@@ -1,8 +1,10 @@
 #!flask/bin/python
 import os
 import unittest
+import test_helper
 
-from app import app
+from app import app, helpers
+
 
 class TestCase(unittest.TestCase):
     def setUp(self):
@@ -10,9 +12,15 @@ class TestCase(unittest.TestCase):
         self.app = app.test_client()
 
 
-    def test_locate_user_on_load(self):
-        rv = self.app.get('/')
-        assert 'Hello, World!' in rv.data
+    # def test_locate_user_on_load(self):
+    #     rv = self.app.get('/')
+    #     assert 'Hello, World!' in rv.data
+
+    def test_helpers_find_spots(self):
+    	potential_spots = test_helper.example_response
+    	close_spots = helpers.find_spots(origin,potential_spots)
+    	assert close_spots.length == 1
+
 
 if __name__ == '__main__':
     unittest.main()
