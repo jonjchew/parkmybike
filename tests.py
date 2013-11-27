397e6675a5aa5fa71cfe2254264c8d3f3e8cfaa7
Jonathan Chew <jon.j.chew@gmail.com>
2013-11-26 17:35:02 -0800

    Refactors test so variables are not being redefined

--- tests.py
+++ tests.py
@@ -5,35 +5,31 @@ import test_helper
 
 from app import app, helpers
 
+origin = [0,0]
+potential_spots = test_helper.example_response
+max_distance = 0.5
 
 class TestCase(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         self.app = app.test_client()
 
-
     # def test_locate_user_on_load(self):
     #     rv = self.app.get('/')
     #     assert 'Hello, World!' in rv.data
 
     def test_helpers_find_spots(self):
-    	potential_spots = test_helper.example_response
-    	origin = [0,0]
     	close_spots = helpers.find_spots(origin, potential_spots)
     	assert len(close_spots) == 1
 
     def test_helpers_filter_spots(self):
-    	origin = [0,0]
-    	all_spots = test_helper.example_response
-    	max_distance = 0.5
-    	close_spots = helpers.filter_spots(origin, all_spots, max_distance)
+    	close_spots = helpers.filter_spots(origin, potential_spots, max_distance)
     	assert len(close_spots) == 1
 
     def test_helpers_valid_spot(self):
-    	max_distance = 0.5
     	valid_distance = 0.4
-    	valid_spot = test_helper.valid_spot
     	invalid_distance = 0.6
+    	valid_spot = test_helper.valid_spot
     	invalid_spot = test_helper.invalid_spot
     	invalid_spot2 = test_helper.invalid_spot2
     	assert helpers.valid_spot(valid_spot, valid_distance, max_distance) == True
@@ -42,7 +38,6 @@ class TestCase(unittest.TestCase):
     	assert helpers.valid_spot(invalid_spot2, valid_distance, max_distance) == None
 
     def test_helpers_get_distance(self):
-    	origin = [0,0]
     	destination = [0,1]
     	assert helpers.get_distance(origin, destination) == 69.17032342836163
 
@@ -50,7 +45,6 @@ class TestCase(unittest.TestCase):
     	array = [1,1,1,2,2,3,3,3,4,4,5,5]
     	unique_array = [1,2,3,4,5]
     	assert helpers.uniqify(array) == unique_array
-
 
 if __name__ == '__main__':
     unittest.main()
